f1a3709b31d0ca37e7e2159760ed5645504b23b8
fangcongyang <765463281@qq.com>
2025-01-06 01:19:03 +0800

    feat(): 新增可取消请求工具类

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,41 @@
 export default {
     trimAll(ele) {
-        if (typeof ele === 'string') {
-            return ele.split(/[\t\r\f\n\s]*/g).join('');
+        if (typeof ele === "string") {
+            return ele.split(/[\t\r\f\n\s]*/g).join("");
         } else {
-            console.error("`${type of ele}` is not a string")
+            console.error("`${type of ele}` is not a string");
         }
     },
 
     isJSON(str) {
-        if (typeof str !== 'string') {
-          return false;
+        if (typeof str !== "string") {
+            return false;
         }
         var regex = /^[\],:{}\s]*$/;
-        return regex.test(str.replace(/\\["\\\/bfnrtu]/g, '@')
-          .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
-          .replace(/(?:^|:|,)(?:\s*\[)+/g, ''));
-    }
-}
+        return regex.test(
+            str
+                .replace(/\\["\\\/bfnrtu]/g, "@")
+                .replace(
+                    /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
+                    "]"
+                )
+                .replace(/(?:^|:|,)(?:\s*\[)+/g, "")
+        );
+    },
+
+    createCancelableRequest(requestFn) {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        const cancelableRequest = async (config) => {
+            return requestFn({ ...config, signal });
+        };
+
+        const cancel = () => {
+            controller.abort();
+            console.log("Request canceled");
+        };
+
+        return { cancelableRequest, cancel };
+    },
+};
